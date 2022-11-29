b6fd97f102743e834258b11dccfdaa295fa6b44d
nvaltekar <nvaltekar@email.arizona.edu>
2022-11-29 13:56:08 -0700

    created library creator class

---
+++ main.py
@@ -0,0 +1,19 @@
+# from BFGS import BFGS
+# from CG import CG
+# from GM import GM
+from system import system_creator
+import numpy as np
+
+def main():
+
+    states = np.random.rand(1000,3)
+    cols = 5 
+    comb = True
+    sys = system_creator(cols,states,comb)
+    
+
+
+if __name__=="__main__":
+    main()
+
+
---
+++ system.py
@@ -0,0 +1,109 @@
+import torch 
+import numpy as np
+from scipy.special import comb
+from sympy import *
+from sympy import symbols, Poly
+import pandas as pd
+
+class system_creator:
+
+    def __init__(self,cols,states,comp=False,tuning=False,n_degree=3) -> None:
+       
+       self.cols = cols
+       self.states = states
+       self.comp = comp
+       self.tuning = tuning
+       self.n_degree = n_degree
+       
+       #create library
+       self.lib = self.create()
+       print(list(self.lib))
+
+    def create(self):
+        
+        '''
+        Consider only polynomial functions , trigonometric, exponential and logaritmic functions 
+        Dictionary without composition is a matrix with matrices:
+        polinomial | sin(polinomial)|cos(polinomial)|tan(polinomial)|exp(polinomial)|log(polinomial)
+        Dictionary with composition has the following size: choose number of compositions out of 6 functions
+        '''
+
+        pol_comb = int(comb(self.n_degree+self.states.shape[1],self.n_degree)-1)
+        base_matrix_size = pol_comb*6
+        if self.comp:
+            compositions=2
+            max_size = base_matrix_size
+            while max_size<self.states.shape[0]:
+                max_size = comb(base_matrix_size,compositions)
+                compositions+=1
+            
+        print("number of compositions is:",compositions)
+        lib = self.compose(compositions,pol_comb,self.states,self.n_degree)
+        return lib
+        
+    def compose(self,compositions,matrix_cols,states,degree):
+        
+        base = np.zeros((states.shape[0],matrix_cols))
+        string = "x:"+str(states.shape[1])
+        variables = symbols(string)
+        exp = variables[0]
+        
+        for var in variables[1:]:
+            exp = var+exp
+        dictionary = self.pol(exp,degree,variables,states)
+        
+        mat_pol = pd.DataFrame.from_dict(dictionary)
+        mat_cos = np.cos(mat_pol)
+        mat_tan = np.tan(mat_pol)
+        mat_sin = np.sin(mat_pol)
+        mat_exp = np.exp(mat_pol)
+        mat_log = np.log(mat_pol)
+        elems = ['pol','sin','cos','tan','exp','log']
+        base_dict = dict(zip(elems,[mat_pol,mat_sin,mat_cos,mat_tan,mat_exp,mat_log]))
+        combinations = [(elems[i],elems[j]) for i in range(len(elems)) for j in range(len(elems))]
+        combinations = combinations[1:]
+        base_dict = self.compose_dict(base_dict,combinations,degree,compositions)
+        return base_dict
+
+
+    def pol(self,exp,degree,variables,states):
+        dictionary = {}
+        for i in range(degree):
+            p = Poly((exp)**(i+1))
+            prods = [prod(x**k for x, k in zip(p.gens, mon)) for mon in p.monoms()]
+            for term in prods:
+                vector = np.zeros(states.shape[0])
+                symbols_list = list(term.free_symbols)
+                indexes = [variables.index(var) for var in symbols_list]
+                for j in range(len(states)):
+                    replace = [(symbols_list[i],states[j][indexes[i]]) for i in range(len(symbols_list))]
+                    vector[j] = term.subs(replace)
+                dictionary[term] = vector
+        return dictionary
+    
+    def compose_dict(self,base_dict,combinations,degree,compositions):
+        for pair in combinations:
+            first = pair[0]
+            second = pair[1]
+            if first == "pol":
+                '''
+                string = "x:"+str(base_dict[second].shape[1])
+                variables = symbols(string)
+                exp = variables[0]
+                for var in variables[1:]:
+                    exp = var+exp
+                base_dict[pair]=self.pol(exp,degree,variables,np.array(base_dict[second]))
+                '''
+                pass
+            elif first == "sin":
+                base_dict[pair]=np.sin(base_dict[second])
+            elif first == "cos":
+                base_dict[pair]=np.cos(base_dict[second])
+            elif first == "tan":
+                base_dict[pair]=np.tan(base_dict[second])
+            elif first == "exp":
+                base_dict[pair]=np.exp(base_dict[second])
+            elif first == "log":
+                base_dict[pair]=np.log(base_dict[second])
+            
+        return base_dict
