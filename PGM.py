6055145c6c7a0259a390a972ba6b6f3f9268b56c
Cornejo <samuelcornejo@arizona.edu>
2022-12-01 15:36:24 -0700

    working?

--- PGM.py
+++ PGM.py
@@ -9,19 +9,14 @@ class PGM_solver:
       self.system.lib = np.nan_to_num(self.system.lib,nan=0,posinf=0,neginf=0)
       self.initial_guess = initial_guess
       self.b = b
-
    
    def backtracking(self,x,grad_f,g_prox,lip,rho,lamb): 
-      print("started backtracking")
-      gamma = 1
+      gamma = 1/lip
       A=self.system.lib
       b=self.b
-
-      while np.linalg.norm(grad_f(A,g_prox(x-gamma*grad_f(A,x,b),lamb),b)-grad_f(A,x,b))>= 1/2*gamma*np.linalg.norm(g_prox(x-gamma*grad_f(A,x,b),lamb)-x):
+      while np.linalg.norm(grad_f(A,g_prox(x-gamma*grad_f(A,x,b),lamb,gamma),b)-grad_f(A,x,b))>1/(2*gamma)*np.linalg.norm(g_prox(x-gamma*grad_f(A,x,b),lamb,gamma)-x):
          gamma = gamma*rho
-         x = g_prox(x-gamma*grad_f(A,x,b),lamb)
-         print(np.linalg.norm(grad_f(A,g_prox(x-gamma*grad_f(A,x,b),lamb),b)-grad_f(A,x,b)))
-      print("finished backtracking")         
+         x = g_prox(x-gamma*grad_f(A,x,b),lamb,gamma)
       return gamma
          
    def PGMD(self,f,grad_f,g_prox,lamb,rho,maxiter,epsilon):
@@ -29,18 +24,21 @@ class PGM_solver:
       x = self.initial_guess
       A = self.system.lib
       b = self.b
-      
-      A2 = np.matmul(A.T,A)
+      A2 = 2*np.matmul(A.T,A)/len(x)
       A2 = np.nan_to_num(A2,nan=0,posinf=0,neginf=0)
       lip=np.real((np.max(np.linalg.eig(A2)[0])))
-      gamma = 1
+      gamma = 1/lip
       num=0
-      while(1/gamma*np.linalg.norm(g_prox(x-gamma*grad_f(A,x,b),lamb)-x))>= epsilon:
-         
+      obj_function=[]
+      #for k in range(int(maxiter)):
+      while(1/gamma*np.linalg.norm(g_prox(x-gamma*grad_f(A,x,b),lamb,gamma)-x))> epsilon:
          gamma = self.backtracking(x,grad_f,g_prox,lip,rho,lamb)
-         x = g_prox(x-gamma*grad_f(A,x,b),lamb)
-         num+=1
-         print("one_step",f(A,x,b))
-      print("finished") 
-
-      return x,f(A,x,b)   
+         x = g_prox(x-gamma*grad_f(A,x,b),lamb,gamma)
+         obj_function.append(f(A,x,b))
+         print(x)
+      print("final_loss",f(A,x,b))
+      print("if x is 0 ",np.sum(np.power(b,2))/len(x))
+      print("least squares solution ",f(A,np.linalg.lstsq(A,b,rcond=None)[0],b)) 
+      print("pseudo inverse solution ",f(A,np.matmul(np.linalg.pinv(A),b),b)) 
+      print("finished")
+      return x,obj_function
