c6fc6213270d3966fb9c6671835a2ecf9f5083f7
Cornejo <samuelcornejo@arizona.edu>
2022-12-06 13:38:17 -0700

    finished

--- PGM.py
+++ PGM.py
@@ -15,29 +15,46 @@ class PGM_solver:
       gamma = 2/lip
       A=self.system.lib
       b=self.b
+      
       while np.linalg.norm(grad_f(A,g_prox(x-gamma*grad_f(A,x,b),lamb,gamma),b)-grad_f(A,x,b))>1/(2*gamma)*np.linalg.norm(g_prox(x-gamma*grad_f(A,x,b),lamb,gamma)-x):
          gamma = gamma*rho
          x = g_prox(x-gamma*grad_f(A,x,b),lamb,gamma)
          
       return gamma
+   
+   def backtracking_fista(self,x,grad_f,g_prox,lip,rho,lamb): 
+      
+      y = x
+      gamma = 2/lip
+      A=self.system.lib
+      b=self.b
+      xk_1 = x
+      tk_1 = 1
+      
+      while np.linalg.norm(grad_f(A,g_prox(y-gamma*grad_f(A,y,b),lamb,gamma),b)-grad_f(A,y,b))>1/(2*gamma)*np.linalg.norm(g_prox(y-gamma*grad_f(A,y,b),lamb,gamma)-y):
+         gamma = gamma*rho
+         xk = g_prox(y-gamma*grad_f(A,y,b),lamb,gamma)
+         tk = (1 + np.sqrt(1+4*tk_1**2))/2
+         y = xk + ((tk_1-1)/tk)*(xk-xk_1) 
+         tk_1 = tk 
+         xk_1 = xk
+         #print("fista gamma update")
+      
+      return gamma
          
-   def PGMD(self,f,grad_f,g_prox,lamb,rho,maxiter,epsilon):
+   def PGMD(self,f,grad_f,g_prox,lamb,rho,lip,maxiter,epsilon):
 
       x = self.initial_guess
       A = self.system.lib
       b = self.b
-      A2 = 2*np.matmul(A.T,A)/len(x)
-      A2 = np.nan_to_num(A2,nan=0,posinf=0,neginf=0)
-      lip = 1
-      #lip=np.real((np.max(np.linalg.eig(A2)[0])))
       gamma = 1
       obj_function=[]
       time_per_step = []
       active_columns = []
       optimality_gap = []
       
-      for k in range(int(maxiter)):
-      #while(1/gamma*np.linalg.norm(g_prox(x-gamma*grad_f(A,x,b),lamb,gamma)-x)) > epsilon:
+      #for k in range(int(maxiter)):
+      while(1/gamma*np.linalg.norm(g_prox(x-gamma*grad_f(A,x,b),lamb,gamma)-x)) > epsilon:
          current= time.time()
          gamma = self.backtracking(x,grad_f,g_prox,lip,rho,lamb)
          x = g_prox(x-gamma*grad_f(A,x,b),lamb,gamma)
@@ -46,31 +63,27 @@ class PGM_solver:
          time_per_step.append(final-current)
          active_columns.append(np.nonzero(x))
          optimality_gap.append(1/gamma*np.linalg.norm(g_prox(x-gamma*grad_f(A,x,b),lamb,gamma)-x)-epsilon)
-         print(optimality_gap[-1],"regular")   
+         #print(optimality_gap[-1],"regular")   
       return x,obj_function,time_per_step,active_columns,optimality_gap
    
 
-   def PGMD_FISTA(self,f,grad_f,g_prox,lamb,rho,maxiter,epsilon):
+   def PGMD_FISTA(self,f,grad_f,g_prox,lamb,rho,lip,maxiter,epsilon):
       y = self.initial_guess
       A = self.system.lib
       b = self.b
-      A2 = 2*np.matmul(A.T,A)/len(y)
-      A2 = np.nan_to_num(A2,nan=0,posinf=0,neginf=0)
-      lip = 1
-      #lip=np.real((np.max(np.linalg.eig(A2)[0])))
-      
       gamma = 1
-      obj_function=[]
       
-      xk_1 = y
+      xk_1 = self.initial_guess
       tk_1 = 1
+      
+      obj_function=[]
       time_per_step = []
       active_columns = []
       optimality_gap = []
-      for k in range(int(maxiter)):
-      #while(1/gamma*np.linalg.norm(g_prox(y-gamma*grad_f(A,y,b),lamb,gamma)-y)) > epsilon:
+      #for k in range(int(maxiter)):
+      while(1/gamma*np.linalg.norm(g_prox(y-gamma*grad_f(A,y,b),lamb,gamma)-y)) > epsilon:
          current= time.time()
-         gamma = self.backtracking(xk_1,grad_f,g_prox,lip,rho,lamb) 
+         gamma = self.backtracking_fista(y,grad_f,g_prox,lip,rho,lamb) 
          xk = g_prox(y-gamma*grad_f(A,y,b),lamb,gamma)
          tk = (1 + np.sqrt(1+4*tk_1**2))/2
          y = xk + ((tk_1-1)/tk)*(xk-xk_1) 
@@ -81,7 +94,7 @@ class PGM_solver:
          time_per_step.append(final-current)
          active_columns.append(np.nonzero(xk))
          optimality_gap.append(1/gamma*np.linalg.norm(g_prox(xk-gamma*grad_f(A,xk,b),lamb,gamma)-xk)-epsilon)
-         print(optimality_gap[-1],"fista")
-         #print(np.round(f(A,xk,b),3),np.nonzero(xk),np.round(1/gamma*np.linalg.norm(g_prox(y-gamma*grad_f(A,y,b),lamb,gamma)-y),3))
+         #print(optimality_gap[-1],"fista")
+         #print(np.round(f(A,y,b),3),np.round(1/gamma*np.linalg.norm(g_prox(y-gamma*grad_f(A,y,b),lamb,gamma)-y),3))
 
       return xk,obj_function,time_per_step,active_columns,optimality_gap
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # from BFGS import BFGS
 # from CG import CG
 from PGM import PGM_solver
+import pandas as pd
 from system import system_creator
 import numpy as np
 from mpl_toolkits import mplot3d
@@ -10,6 +11,11 @@ import seaborn as sns
 import time
 
 from scipy.integrate import solve_ivp
+
+
+def addlabels(x,y):
+    for i in range(len(x)):
+        plt.text(i,y[i],y[i])
 
 
 def f(A,x,b):
@@ -34,7 +40,7 @@ def lorentz_system(t,alpha,sigma,rho,beta):
     
 def main():
 
-    n_points =[100]
+    n_points =[100,200,500]
     hertz = 10
     for points in n_points:
         
@@ -49,9 +55,6 @@ def main():
         states=sol.y.T
         diff = np.zeros((states.shape))
         diff[1:,:] = np.diff(states,axis=0)/(1/hertz)        
-        # states,diff = lorentz_system(sigma,rho,beta,points)
-        #states,states_norm = normalize(states,return_norm=True)
-        # diff = np.nan_to_num(diff,nan=0,posinf=0,neginf=0)
         diff,diff_norm = normalize(diff,return_norm=True)
         
         
@@ -64,9 +67,12 @@ def main():
         
         #set parameters
         initial_guess = np.zeros((sys.lib.shape[1],states.shape[1]))
+        A2 = 2*np.matmul(sys.lib.T,sys.lib)/len(initial_guess)
+        A2 = np.nan_to_num(A2,nan=0,posinf=0,neginf=0)
+        lip=np.real((np.max(np.linalg.eig(A2)[0])))
         rho = 0.5
         lamb = 5e-3
-        maxiter = 1e5
+        maxiter = 1e4
         epsilon = 1e-4
 
         #create storage
@@ -75,34 +81,36 @@ def main():
         time_per_step = {"reg":{},"fista":{}}
         active_columns = {"reg":{},"fista":{}}
         opt_gap = {"reg":{},"fista":{}}
-        
+        indexes = np.arange(len(labels))+0.3
         sns.set()
         for col in range(3):
             pgm = PGM_solver(sys,initial_guess[:,col],diff[:,col])
-            solution['reg'][col] ,obj_func['reg'][col],time_per_step['reg'][col],active_columns['reg'][col],opt_gap['reg'][col]=pgm.PGMD(f,grad_f,g_prox,lamb,rho,maxiter,epsilon)
+            solution['reg'][col] ,obj_func['reg'][col],time_per_step['reg'][col],active_columns['reg'][col],opt_gap['reg'][col]=pgm.PGMD(f,grad_f,g_prox,lamb,rho,lip,maxiter,epsilon)
             print(solution['reg'][col])
             sparse_index = (np.nonzero(solution['reg'][col]))[0]
             for index in sparse_index:
                 print("sparse representation for equation %d found by PGMB :%s " %(col,labels[index]))
-            plt.figure()
-            plt.plot(sys.lib@solution['reg'][col],'o')
-            plt.plot(diff[:,col],'x')
-            plt.legend(["prediction","real"])
-            plt.show()
             print("average time per step by PGMD ",np.mean(time_per_step["reg"][col]))
             print("MSE found by PGMB :",obj_func['reg'][col][-1])
-            solution['fista'][col] ,obj_func['fista'][col],time_per_step['fista'][col],active_columns['fista'][col],opt_gap['fista'][col]=pgm.PGMD_FISTA(f,grad_f,g_prox,lamb,rho,maxiter,epsilon)
+            solution['fista'][col] ,obj_func['fista'][col],time_per_step['fista'][col],active_columns['fista'][col],opt_gap['fista'][col]=pgm.PGMD_FISTA(f,grad_f,g_prox,lamb,rho,lip,maxiter,epsilon)
             print(solution['fista'][col])
             sparse_index = (np.nonzero(solution['fista'][col]))[0]
             for index in sparse_index:
                 print("sparse representation for equation %d found by PGMB FISTA:%s " %(col,labels[index]))
             print("average time per step by PGMD FISTA",np.mean(time_per_step["fista"][col]))
             print("MSE found by PGMB FISTA:",obj_func['fista'][col][-1])
-            plt.figure()
-            plt.plot(sys.lib@solution['fista'][col],'o')
-            plt.plot(diff[:,col],'x')
-            plt.legend(["prediction","real"])
-            plt.show()
+            
+            plt.figure(figsize=(20,10))
+            plt.plot(np.multiply(sys.lib@solution['reg'][col],lib_norm[col]),'o')
+            plt.plot(np.multiply(sys.lib@solution['fista'][col],lib_norm[col]),'o')
+            plt.plot(np.multiply(diff[:,col],diff_norm[col]),'x')
+            
+            plt.ylabel("state_derivative_"+str(col))
+            plt.title("State %s Derivative Regression " %(str(col)))
+            plt.legend(["prediction PGMD","prediction FISTA","real"])
+            plt.savefig("states_"+str(col)+"_regression_"+str(points)+".png")
+            plt.close()
+            
             plt.figure(figsize=(20,10))
             plt.semilogy(obj_func['reg'][col])
             plt.semilogy(obj_func['fista'][col])
@@ -113,16 +121,21 @@ def main():
             plt.savefig("equation_pred_"+str(col)+str(points)+".png")
             plt.close()
 
-            plt.figure(figsize=(20,10))
-            active_columns['reg'][col]=[len(i) for i in active_columns['reg'][col]]
-            active_columns['reg'][col],bins=np.unique(active_columns['reg'][col],return_counts=True)
-            plt.bar(x=bins,height=active_columns['reg'][col])
-            active_columns['fista'][col]=[len(i) for i in active_columns['fista'][col]]
-            active_columns['fista'][col],bins=np.unique(active_columns['fista'][col],return_counts=True)
-            plt.bar(x=bins,height=active_columns['fista'][col])
+            df_reg = pd.DataFrame([labels,solution['reg'][col]]).T
+            df_reg.columns = ['label','value']
+            df_reg['solver']='pgmd'
+            df_reg.value = np.abs(df_reg.value)
+            lengths=np.arange(len(df_reg))
+            df_fista = pd.DataFrame([labels,solution['fista'][col]]).T
+            df_fista.columns =['label','value']
+            df_fista['solver']='pgmd-fista'
+            df_fista.value = np.abs(df_fista.value)
             
-            plt.ylabel('number of active terms')
-            plt.xlabel('number of iterations')
+            plt.figure(figsize=(20,10))
+            plt.bar(x=lengths,height=df_reg['value'],alpha=0.5,tick_label=df_reg['label'])
+            plt.bar(x=lengths,height=df_fista['value'],alpha=0.5,tick_label=df_reg['label'])
+            plt.ylabel('absolute value of active terms')
+            plt.xlabel('type of term')
             plt.title("equation_pred_"+str(col)+"active_terms")
             plt.legend(["regular PGMD","FISTA PGMD"])
             plt.savefig("active_terms_equation_pred_"+str(col)+str(points)+".png")
